fcf648e02c3c0f4f4e960ebe05dc02c578a2b95b
Kazi Abdur Rakib <57065763+karakib2k18@users.noreply.github.com>
2023-02-04 15:34:08 +0600

    add user pass

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const ObjectId = require("mongodb").ObjectId;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3zctf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://jewelrydDB:EwcZB6uJI9uPg38B@cluster0.3zctf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3zctf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
